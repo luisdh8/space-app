7bc40b6344407a7c27eddadb908d173f75e33f36
Luis Díaz <luisdh.8@outlook.com>
2025-01-03 14:14:39 -0600

    Aula 1

---
+++ space-app/src/components/Cabecera/index.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const HeaderEstilizado = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+`
+
+
+const Cabecera = () => {
+    return <HeaderEstilizado>
+        <img src="" alt="" />
+    </HeaderEstilizado>
+};
+
+export default Cabecera;
